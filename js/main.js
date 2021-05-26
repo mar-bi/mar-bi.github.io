e2b8c292e6cc2564c127131b0c9fc6064b515889
Marina Biletska <marina.biletska@gmail.com>
2021-05-26 16:13:02 +0200

    Update photo and 'about me' text

--- js/main.js
+++ js/main.js
@@ -1,17 +1,1 @@
-// const values = [0.90, 0.92, 0.87, 0.95];
-// values.map((elem, index) => {
-//   const canvas = document.getElementById(`skill-${index}`),
-//     ctx = canvas.getContext("2d");
 
-//   const clipArc =  (elem - 0.75) * 2;
-
-//   ctx.beginPath();
-//   ctx.arc(50, 50, 40, (3 /2)*Math.PI, (Math.PI + clipArc* Math.PI));
-//   ctx.strokeStyle =  '#e57c22';
-//   ctx.lineWidth = 10;
-//   ctx.stroke();
-
-//   ctx.font = 'bold 16px Helvetica';
-//   ctx.fillStyle = '#e57c22';
-//   ctx.fillText(`${elem*100}%`, 40, 55);
-// });
